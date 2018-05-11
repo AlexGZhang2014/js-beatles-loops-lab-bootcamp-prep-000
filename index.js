d39d00f33d26add7e21af720151fe380f5b78671
Alex Zhang <alexgzhang92@gmail.com>
2018-05-11 22:19:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(arrMusic, arrInst) {
   let array = [];
   for (let i = 0; i < arrMusic.length; i++) {
-    
+    array.push(`${arrMusic[i]} plays ${arrInst[i]}`);
   }
+  return array;
 }
